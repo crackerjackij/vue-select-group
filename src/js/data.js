583da524552b9acfd2b7f80900789247b19c9718
crackerjackij <saltkeeper@hanmail.net>
2020-11-05 17:23:51 +0900

    Update data.js

--- src/js/data.js
+++ src/js/data.js
@@ -1,15 +1,15 @@
 var vsg = new Vue({
 	el: '#vue_root',
 	data: {
-      datas: [
-            {g_title: '2018', g_datas: [
-              {data: '20180101'},
-              {data: '20180102'}
-            ]},
-            {g_title: '2019', g_datas: [
-              {data: '20190101'},
-              {data: '20190102'}
-            ]}
-          ]
-	}
+	      datas: [
+		    {g_title: '2018', g_datas: [
+		      {data: '20180101'},
+		      {data: '20180102'}
+		    ]},
+		    {g_title: '2019', g_datas: [
+		      {data: '20190101'},
+		      {data: '20190102'}
+		    ]}
+		  ]
+		}
 });
